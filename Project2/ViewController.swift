3a6c9c753003d5d86396038b8577ab3dbee8dede
EPJrWorld <edwin.przezwiecki.jr@icloud.com>
2022-04-26 03:21:41 +0200

    Project_6a addition
    The app has been made compatible with both portrait and landscape orientations in terms of Project_6a. And by the way, I have finally managed to resolve an issue with borders around the buttons!

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -4,7 +4,6 @@ extension String {
     func capitalizingFirstLetter() -> String {
         return prefix(1).capitalized + dropFirst()
     }
-
     mutating func capitalizeFirstLetter() {
         self = self.capitalizingFirstLetter()
     }
